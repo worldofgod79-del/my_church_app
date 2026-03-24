7f9b48da7750ec6c79e2922f3fbe25f1b6d6c7d4
worldofgod79-del <worldofgod79@gmail.com>
2026-03-24 23:10:50 +0530

    Update app_routes.dart

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,11 +1,21 @@
 import 'package:go_router/go_router.dart';
 import '../features/splash/splash_screen.dart';
 import '../features/home/home_screen.dart';
+import '../features/bible/bible_home.dart';
+import '../features/bible/bible_reader.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
     GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/bible', builder: (context, state) => const BibleHome()),
+    GoRoute(
+      path: '/bible-reader/:id',
+      builder: (context, state) {
+        final id = int.parse(state.pathParameters['id']!);
+        return BibleReader(bookId: id);
+      },
+    ),
   ],
 );
