2ee7119c99d457d099a7eb2986bb531f8b5125ea
worldofgod79-del <worldofgod79-del@users.noreply.github.com>
2026-03-25 05:13:25 +0000

    deploy: 0423873e09ce1e27d2340b33687f479c0e11a1fb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "394471914" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3979944083" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
