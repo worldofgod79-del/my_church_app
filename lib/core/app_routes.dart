9b0a6e7d059ced473cfd595f1860ec48bd120c3e
worldofgod79-del <worldofgod79@gmail.com>
2026-03-26 21:54:53 +0530

    Update app_routes.dart

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,29 +1,5 @@
-import 'package:go_router/go_router.dart';
-import '../features/splash/splash_screen.dart';
-import '../features/home/home_screen.dart';
-import '../features/bible/bible_home.dart';
-import '../features/bible/bible_reader.dart';
-import '../features/bible/bible_search.dart';
+import '../features/admin/admin_dashboard.dart';
 
-final GoRouter router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
-    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
-    GoRoute(path: '/bible', builder: (context, state) => const BibleHome()),
-    GoRoute(
-      path: '/search',
-      builder: (context, state) => BibleSearch(initialBook: state.uri.queryParameters['book']),
-    ),
-    GoRoute(
-      path: '/bible-reader/:name',
-      builder: (context, state) {
-        return BibleReader(
-          bookName: state.pathParameters['name']!,
-          initialChapter: state.uri.queryParameters['chapter'],
-          initialVerse: state.uri.queryParameters['verse'],
-        );
-      },
-    ),
-  ],
-);
+// Routes లో యాడ్ చెయ్:
+GoRoute(path: '/admin-login', builder: (context, state) => const AdminLoginScreen()),
+GoRoute(path: '/admin-dashboard', builder: (context, state) => const AdminDashboard()),
