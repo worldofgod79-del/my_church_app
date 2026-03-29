bb792c07f0e213f3d2c9d49886f04f679fd43c71
worldofgod79-del <worldofgod79@gmail.com>
2026-03-29 22:08:09 +0530

    Update bible_service.dart

--- lib/features/bible/bible_service.dart
+++ lib/features/bible/bible_service.dart
@@ -3,6 +3,8 @@ import 'package:xml/xml.dart';
 
 class BibleService {
   static Map<String, Map<String, dynamic>>? _bibleCache;
+  // వచనానికి ID ని స్టోర్ చేయడానికి Map (Key: "Book_Chap_Verse")
+  static Map<String, int>? _verseIdMap;
 
   final List<String> bookNames = [
     "ఆదికాండము", "నిర్గమకాండము", "లేవీయకాండము", "సంఖ్యాకాండము", "ద్వితీయోపదేశకాండము",
@@ -20,21 +22,20 @@ class BibleService {
     "2పేతురు", "1యోహాను", "2యోహాను", "3యోహాను", "యూదా", "ప్రకటన గ్రంథం"
   ];
 
-  // క్రాస్ రిఫరెన్స్ నావిగేషన్ కోసం Mapping
   final Map<String, String> engToTelMapping = {
-    "Gen": "ఆదికాండము", "Exo": "నిర్గమకాండము", "Lev": "లేవీయకాండము", "Num": "సంఖ్యాకాండము", "Deu": "ద్వితీయోపదేశకాండము",
-    "Jos": "యెహోషువ", "Jud": "న్యాయాధిపతులు", "Rut": "రూతు", "1Sa": "1సమూయేలు", "2Sa": "2సమూయేలు",
-    "1Ki": "1రాజులు", "2Ki": "2రాజులు", "1Ch": "1దినవృత్తాంతములు", "2Ch": "2దినవృత్తాంతములు", "Ezr": "ఎజ్రా",
-    "Neh": "నెహెమ్యా", "Est": "ఎస్తేరు", "Job": "యోబు", "Psa": "కీర్తనలు", "Pro": "సామెతలు",
-    "Ecc": "ప్రసంగి", "Son": "పరమగీతము", "Isa": "యెషయా", "Jer": "యిర్మీయా", "Lam": "విలాపవాక్యములు",
-    "Eze": "యెహెజ్కేలు", "Dan": "దానియేలు", "Hos": "హోషేయ", "Joe": "యోవేలు", "Amo": "ఆమోసు",
-    "Oba": "ఓబద్యా", "Jon": "యోనా", "Mic": "మీకా", "Nah": "నహూము", "Hab": "హబక్కూకు",
-    "Zep": "జెఫన్యా", "Hag": "హగ్గయి", "Zec": "జకర్యా", "Mal": "మలాకీ", "Mat": "మత్తయి",
-    "Mar": "మార్కు", "Luk": "లూకా", "Joh": "యోహాను", "Act": "అపో.కార్యములు", "Rom": "రోమీయులకు",
-    "1Co": "1కొరింథీయులకు", "2Co": "2కొరింథీయులకు", "Gal": "గలతీయులకు", "Eph": "ఎఫెసీయులకు", "Phi": "ఫిలిప్పీయులకు",
-    "Col": "కొలొస్సయులకు", "1Th": "1థెస్సలొనీకయులకు", "2Th": "2థెస్సలొనీకయులకు", "1Ti": "1తిమోతికి", "2Ti": "2తిమోతికి",
-    "Tit": "తీతుకు", "Phm": "ఫిలేమోనుకు", "Heb": "హెబ్రీయులకు", "Jam": "యాకోబు", "1Pe": "1పేతురు",
-    "2Pe": "2పేతురు", "1Jo": "1యోహాను", "2Jo": "2యోహాను", "3Jo": "3యోహాను", "Jud": "యూదా", "Rev": "ప్రకటన గ్రంథం"
+    "GEN": "ఆదికాండము", "EXO": "నిర్గమకాండము", "LEV": "లేవీయకాండము", "NUM": "సంఖ్యాకాండము", "DEU": "ద్వితీయోపదేశకాండము",
+    "JOS": "యెహోషువ", "JDG": "న్యాయాధిపతులు", "RUT": "రూతు", "1SA": "1సమూయేలు", "2SA": "2సమూయేలు",
+    "1KI": "1రాజులు", "2KI": "2రాజులు", "1CH": "1దినవృత్తాంతములు", "2CH": "2దినవృత్తాంతములు", "EZR": "ఎజ్రా",
+    "NEH": "నెహెమ్యా", "EST": "ఎస్తేరు", "JOB": "యోబు", "PSA": "కీర్తనలు", "PRO": "సామెతలు",
+    "ECC": "ప్రసంగి", "SNG": "పరమగీతము", "ISA": "యెషయా", "JER": "యిర్మీయా", "LAM": "విలాపవాక్యములు",
+    "EZK": "యెహెజ్కేలు", "DAN": "దానియేలు", "HOS": "హోషేయ", "JOL": "యోవేలు", "AMO": "ఆమోసు",
+    "OBA": "ఓబద్యా", "JON": "యోనా", "MIC": "మీకా", "NAH": "నహూము", "HAB": "హబక్కూకు",
+    "ZEP": "జెఫన్యా", "HAG": "హగ్గయి", "ZEC": "జకర్యా", "MAL": "మలాకీ", "MAT": "మత్తయి",
+    "MRK": "మార్కు", "LUK": "లూకా", "JOH": "యోహాను", "ACT": "అపో.కార్యములు", "ROM": "రోమీయులకు",
+    "1CO": "1కొరింథీయులకు", "2CO": "2కొరింథీయులకు", "GAL": "గలతీయులకు", "EPH": "ఎఫెసీయులకు", "PHP": "ఫిలిప్పీయులకు",
+    "COL": "కొలొస్సయులకు", "1TH": "1థెస్సలొనీకయులకు", "2TH": "2థెస్సలొనీకయులకు", "1TI": "1తిమోతికి", "2TI": "2తిమోతికి",
+    "TIT": "తీతుకు", "PHM": "ఫిలేమోనుకు", "HEB": "హెబ్రీయులకు", "JAS": "యాకోబు", "1PE": "1పేతురు",
+    "2PE": "2పేతురు", "1JO": "1యోహాను", "2JO": "2యోహాను", "3JO": "3యోహాను", "JUD": "యూదా", "REV": "ప్రకటన గ్రంథం"
   };
 
   List<String> getOTBooks() => bookNames.sublist(0, 39);
@@ -42,26 +43,35 @@ class BibleService {
 
   Future<void> _loadXMLOnce() async {
     if (_bibleCache != null) return;
+    _bibleCache = {};
+    _verseIdMap = {};
+    int globalIdCounter = 1; // 1 నుండి స్టార్ట్ అవుతుంది
+
     try {
       final String rawXml = await rootBundle.loadString('assets/bible.xml');
       final document = XmlDocument.parse(rawXml);
-      Map<String, Map<String, dynamic>> tempCache = {};
+      
       for (var bookNode in document.findAllElements('BIBLEBOOK')) {
-        String bnumber = bookNode.getAttribute('bnumber') ?? "";
-        int bIndex = int.parse(bnumber) - 1;
-        String teluguBookName = bookNames[bIndex];
+        int bNum = int.parse(bookNode.getAttribute('bnumber')!);
+        String telBookName = bookNames[bNum - 1];
         Map<String, dynamic> chaptersMap = {};
+
         for (var chapterNode in bookNode.findAllElements('CHAPTER')) {
-          String cnumber = chapterNode.getAttribute('cnumber')!;
+          String cNum = chapterNode.getAttribute('cnumber')!;
           Map<String, String> versesMap = {};
+
           for (var versNode in chapterNode.findAllElements('VERS')) {
-            versesMap[versNode.getAttribute('vnumber')!] = versNode.innerText.trim();
+            String vNum = versNode.getAttribute('vnumber')!;
+            versesMap[vNum] = versNode.innerText.trim();
+            
+            // Map ID: "ఆదికాండము_1_1" -> 1
+            _verseIdMap!["${telBookName}_${cNum}_$vNum"] = globalIdCounter;
+            globalIdCounter++;
           }
-          chaptersMap[cnumber] = versesMap;
+          chaptersMap[cNum] = versesMap;
         }
-        tempCache[teluguBookName] = {"name": teluguBookName, "chapters": chaptersMap};
+        _bibleCache![telBookName] = {"name": telBookName, "chapters": chaptersMap};
       }
-      _bibleCache = tempCache;
     } catch (e) {
       throw Exception("XML Error: $e");
     }
@@ -70,6 +80,11 @@ class BibleService {
   Future<Map<String, dynamic>> loadBook(String bookName) async {
     await _loadXMLOnce();
     return _bibleCache![bookName] ?? {};
+  }
+
+  // వచనం యొక్క Global ID తెచ్చే ఫంక్షన్
+  int getGlobalId(String b, String c, String v) {
+    return _verseIdMap?["${b}_${c}_$v"] ?? 0;
   }
 }
 
